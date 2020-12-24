6ab5f49fb2906e843db4f1847a37b1368c25bf18
diesekiefer <diesekiefer@example.com>
2020-12-24 23:07:24 +0900

    add index view

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,16 +33,11 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
-            EntityManager em = DBUtil.createEntityManager();
-            List<Tasks> messages = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
-            response.getWriter().append(Integer.valueOf(messages.size()).toString());
-            em.close();
-        } catch (Exception e){
-            System.out.println(e);
-        }
-
-
+        EntityManager em = DBUtil.createEntityManager();
+        List<Tasks> tasks = em.createNamedQuery("getAllTasks", Tasks.class).getResultList();
+        em.close();
+        request.setAttribute("tasks", tasks);
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
-
 }
