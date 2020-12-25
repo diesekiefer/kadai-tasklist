fad52e7fcfe518651324eff5bbf8337bed4273db
diesekiefer <diesekiefer@example.com>
2020-12-25 09:45:07 +0900

    add validation

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -33,7 +33,7 @@ public class NewServlet extends HttpServlet {
         request.setAttribute("_token", request.getSession().getId());
 
         // おまじないとしてのインスタンスを生成
-        request.setAttribute("message", new Tasks());
+        request.setAttribute("task", new Tasks());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
         rd.forward(request, response);
